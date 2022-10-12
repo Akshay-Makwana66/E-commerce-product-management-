bf6e676a15ccc10ebafdd26dc2d9fc276abc3d35
functionup-am <104677275+functionup-am@users.noreply.github.com>
2022-10-12 16:33:49 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,7 +10,7 @@ const Authentication = function (req, res, next) {
         return res.status(401).send({ status: false, message: "Missing authentication token in request " });
       }   
 
-    let token = req.headers.authorization.split(",")[1]
+    let token = req.headers.authorization.split(" ")[1]
 
     const decoded = jwt.decode(token);   
    
